f953ee4361301472d7614f2250299d37e580a642
Erik Perri <erik.perri@gmail.com>
2022-07-23 21:31:48 -0400

    test: Fixed test and added loading check

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,11 +4,31 @@
 
 import 'react-native';
 
-import {render} from '@testing-library/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {act, render} from '@testing-library/react-native';
 import React from 'react';
 
 import App from '../App';
 
-it('renders correctly', () => {
-  render(<App />);
+it('renders correctly', async () => {
+  let capturedResolve: ((key: string | null) => void) | undefined;
+
+  const getItemSpy = jest
+    .spyOn(AsyncStorage, 'getItem')
+    .mockImplementation(() => {
+      return new Promise(resolve => {
+        capturedResolve = resolve;
+      });
+    });
+
+  const {getByText, queryByText} = render(<App />);
+
+  expect(getByText('Loading')).toBeTruthy();
+
+  await act(async () => {
+    capturedResolve?.(null);
+  });
+
+  expect(queryByText('Loading')).toBeNull();
+  expect(getItemSpy).toHaveBeenCalledTimes(1);
 });
